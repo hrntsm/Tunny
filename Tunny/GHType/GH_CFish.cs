dbef5ebb0b855f5be962ad666869062adcf39aa2
hrntsm <contact@hrntsm.com>
2022-05-28 00:11:12 +0900

    Fix GH_CFish r/w method to internalize its data

--- Tunny/GHType/GH_CFish.cs
+++ Tunny/GHType/GH_CFish.cs
@@ -1,7 +1,14 @@
+using System;
 using System.Collections.Generic;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
 using System.Text;
 
+using GH_IO.Serialization;
+
 using Grasshopper.Kernel.Types;
+
+using Newtonsoft.Json;
 
 using Rhino.Geometry;
 
@@ -20,6 +27,22 @@ namespace Tunny.GHType
         public override string TypeDescription => "Tunny optimizer result.";
 
         public override IGH_Goo Duplicate() => new GH_CFish(this);
+
+        public override bool Read(GH_IReader reader)
+        {
+            string base64 = string.Empty;
+            if (reader.TryGetString("CFish_bin", ref base64))
+            {
+                Value = CFish.FromBase64(base64);
+            }
+            return base.Read(reader);
+        }
+
+        public override bool Write(GH_IWriter writer)
+        {
+            writer.SetString("CFish_bin", CFish.ToBase64(Value));
+            return base.Write(writer);
+        }
 
         public override string ToString()
         {
@@ -45,11 +68,40 @@ namespace Tunny.GHType
         }
     }
 
+    [Serializable]
     public class CFish
     {
-        internal int ModelNumber;
-        internal Mesh ModelMesh;
-        internal Dictionary<string, double> Variables;
-        internal Dictionary<string, double> Objectives;
+        public int ModelNumber;
+        public Mesh ModelMesh;
+        public Dictionary<string, double> Variables;
+        public Dictionary<string, double> Objectives;
+
+        public string SerializeToJson()
+        {
+            return JsonConvert.SerializeObject(this, Formatting.Indented);
+        }
+
+        public static CFish DeserializeFromJson(string json)
+        {
+            return JsonConvert.DeserializeObject<CFish>(json);
+        }
+        public static string ToBase64(CFish cFish)
+        {
+            using (var ms = new MemoryStream())
+            {
+                new BinaryFormatter().Serialize(ms, cFish);
+                return Convert.ToBase64String(ms.ToArray());
+            }
+        }
+
+        public static CFish FromBase64(string base64String)
+        {
+            byte[] bytes = Convert.FromBase64String(base64String);
+            using (var ms = new MemoryStream(bytes))
+            {
+                return (CFish)new BinaryFormatter().Deserialize(ms);
+            }
+
+        }
     }
 }
