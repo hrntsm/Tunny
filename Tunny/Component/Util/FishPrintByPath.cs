60bea0e09b4855ad09fa3a856b168b6dcb82404f
hrntsm <contact@hrntsm.com>
2023-05-14 15:26:49 +0900

    Fix FishPrintByPath

--- Tunny/Component/Util/FishPrintByPath.cs
+++ Tunny/Component/Util/FishPrintByPath.cs
@@ -3,9 +3,6 @@ using System.Drawing;
 
 using Grasshopper.Kernel;
 
-using Rhino;
-using Rhino.Display;
-
 using Tunny.Component.Params;
 using Tunny.Resources;
 
@@ -22,7 +19,7 @@ namespace Tunny.Component.Util
 
         protected override void RegisterInputParams(GH_InputParamManager pManager)
         {
-            pManager.AddTextParameter("Path", "Path", "Create Fish Print by file path", GH_ParamAccess.list);
+            pManager.AddTextParameter("Path", "Path", "Create Fish Print by file path", GH_ParamAccess.item);
         }
 
         protected override void RegisterOutputParams(GH_OutputParamManager pManager)
@@ -30,17 +27,16 @@ namespace Tunny.Component.Util
             pManager.AddParameter(new Param_FishPrint(), "FishPrint", "FPrint", "FishPrint", GH_ParamAccess.item);
         }
 
-        private readonly RhinoDoc _doc = RhinoDoc.ActiveDoc;
         protected override void SolveInstance(IGH_DataAccess DA)
         {
-            RhinoView activeView = _doc.Views.ActiveView;
-            Bitmap bitmap = activeView.CaptureToBitmap();
+            string path = string.Empty;
+            DA.GetData(0, ref path);
 
+            var bitmap = Image.FromFile(path) as Bitmap;
             DA.SetData(0, bitmap);
         }
 
         protected override Bitmap Icon => Resource.FishPrintByPath;
-
         public override Guid ComponentGuid => new Guid("8ea46ce9-d546-4fdc-a950-976134af8227");
     }
 }
