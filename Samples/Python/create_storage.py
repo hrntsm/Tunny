73fcc9b2da5aaf9166c1707faf6bc34b2b0e5eaa
NATSUME Hiroaki <hiroaki@nature-architects.com>
2023-06-26 12:23:50 +0900

    Add python create_storage

---
+++ Samples/Python/create_storage.py
@@ -0,0 +1,23 @@
+import optuna
+
+def objective(trial):
+    x = trial.suggest_float("x", -100, 100)
+    return x**2
+
+# SQLite3 storage
+storage = optuna.storages.RDBStorage(
+    url="sqlite:///test.db",
+)
+study_db = optuna.create_study(storage=storage)
+study_db.optimize(objective, n_trials=10)
+
+
+# Journal storage
+file_path = "test.log"
+lock_obj = optuna.storages.JournalFileOpenLock(file_path)
+
+storage = optuna.storages.JournalStorage(
+    optuna.storages.JournalFileStorage(file_path, lock_obj=lock_obj),
+)
+study_journal = optuna.create_study(storage=storage)
+study_journal.optimize(objective, n_trials=10)
