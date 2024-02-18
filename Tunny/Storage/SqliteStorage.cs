31f15ccdba8ebed579deb32b82e71c67e5b2b8d7
hrntsm <contact@hrntsm.com>
2024-02-18 13:41:45 +0900

    Refactor tunny storage to use optuna.csproj rdb storage

--- Tunny/Storage/SqliteStorage.cs
+++ Tunny/Storage/SqliteStorage.cs
@@ -1,11 +1,7 @@
 using System;
-using System.Collections.Generic;
-using System.Data.SQLite;
-using System.Globalization;
-using System.IO;
+using System.Linq;
 
 using Optuna.Study;
-using Optuna.Trial;
 
 using Python.Runtime;
 
@@ -16,89 +12,6 @@ namespace Tunny.Storage
     public class SqliteStorage : PythonInit, IStorage
     {
         public dynamic Storage { get; set; }
-
-        private static bool CheckTableExist(SQLiteConnection connection)
-        {
-            TLog.MethodStart();
-            int hasStudiesTable;
-            using (var command = new SQLiteCommand(connection))
-            {
-                command.CommandText = "SELECT COUNT(*) FROM sqlite_master WHERE type='table' AND name='studies';";
-                hasStudiesTable = Convert.ToInt32(command.ExecuteScalar(), CultureInfo.InvariantCulture);
-            }
-            return hasStudiesTable > 0;
-        }
-
-        private static void GetStudyAttributes(List<StudySummary> studySummaries, SQLiteConnection connection)
-        {
-            TLog.MethodStart();
-            using (var command = new SQLiteCommand(connection))
-            {
-                command.CommandText = "SELECT * FROM study_user_attributes";
-                using (SQLiteDataReader reader = command.ExecuteReader())
-                {
-                    while (reader.Read())
-                    {
-                        long studyId = (long)reader["study_id"];
-                        string key = (string)reader["key"];
-
-                        if (key == "variable_names")
-                        {
-                            string valueJson = (string)reader["value_json"];
-                            string[] values = valueJson.Replace("\"", "").Replace("[", "").Replace("]", "").Replace(" ", "").Split(',');
-                            studySummaries.Find(x => x.StudyId == studyId).UserAttrs.Add(key, values);
-                        }
-                    }
-                }
-            }
-            using (var command = new SQLiteCommand(connection))
-            {
-                command.CommandText = "SELECT * FROM study_system_attributes";
-                using (SQLiteDataReader reader = command.ExecuteReader())
-                {
-                    while (reader.Read())
-                    {
-                        long studyId = (long)reader["study_id"];
-                        string key = (string)reader["key"];
-
-                        if (key == "study:metric_names")
-                        {
-                            string valueJson = (string)reader["value_json"];
-                            string[] values = valueJson.Replace("\"", "").Replace("[", "").Replace("]", "").Replace(" ", "").Split(',');
-                            studySummaries.Find(x => x.StudyId == studyId).SystemAttrs.Add(key, values);
-                        }
-                    }
-                }
-            }
-        }
-
-        private static void GetStudy(List<StudySummary> studySummaries, SQLiteConnection connection)
-        {
-            TLog.MethodStart();
-            using (var command = new SQLiteCommand(connection))
-            {
-                command.CommandText = "SELECT * FROM studies";
-                using (SQLiteDataReader studyReader = command.ExecuteReader())
-                {
-                    while (studyReader.Read())
-                    {
-                        long studyId = (long)studyReader["study_id"];
-                        string studyName = (string)studyReader["study_name"];
-
-                        var summary = new StudySummary(studyName,
-                                                       StudyDirection.NotSet,
-                                                       new Trial(),
-                                                       new Dictionary<string, object>(),
-                                                       new Dictionary<string, object>(),
-                                                       100,
-                                                       DateTime.Now,
-                                                       (int)studyId);
-
-                        studySummaries.Add(summary);
-                    }
-                }
-            }
-        }
 
         public dynamic CreateNewStorage(bool useInnerPythonEngine, Settings.Storage storageSetting)
         {
@@ -144,31 +57,27 @@ namespace Tunny.Storage
         public StudySummary[] GetStudySummaries(string storagePath)
         {
             TLog.MethodStart();
-            var studySummaries = new List<StudySummary>();
-            if (!File.Exists(storagePath))
-            {
-                return studySummaries.ToArray();
-            }
+            var storage = new Optuna.Storage.RDB.SqliteStorage(storagePath);
+            StudySummary[] studySummaries = Study.GetAllStudySummaries(storage);
 
-            var sqliteConnection = new SQLiteConnectionStringBuilder
+            var oldFormatVersion = new Version("0.9.1");
+            foreach (StudySummary studySummary in studySummaries)
             {
-                DataSource = storagePath,
-                Version = 3
-            };
-
-            using (var connection = new SQLiteConnection(sqliteConnection.ToString()))
-            {
-                connection.Open();
-
-                if (!CheckTableExist(connection))
+                string versionString = (studySummary.UserAttrs["tunny_version"] as string[])[0];
+                var version = new Version(versionString);
+                if (version <= oldFormatVersion)
                 {
-                    return studySummaries.ToArray();
+                    UpdateVariableNamesAttr(studySummary);
                 }
-                GetStudy(studySummaries, connection);
-                GetStudyAttributes(studySummaries, connection);
             }
 
-            return studySummaries.ToArray();
+            return studySummaries;
+        }
+
+        private static void UpdateVariableNamesAttr(StudySummary studySummary)
+        {
+            TLog.MethodStart();
+            studySummary.UserAttrs["variable_names"] = (studySummary.UserAttrs["variable_names"] as string[])[0].Split(',').ToArray();
         }
     }
 }
