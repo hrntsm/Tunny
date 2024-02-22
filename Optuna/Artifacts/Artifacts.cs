51994e8b7c8f0d62ddc9222adfae7ee6730adf29
NATSUME Hiroaki <hiroaki@nature-architects.com>
2024-02-22 14:46:55 +0900

    Refactor Artifacts class and add error handling

--- Optuna/Artifacts/Artifacts.cs
+++ Optuna/Artifacts/Artifacts.cs
@@ -1,13 +1,40 @@
-using Python.Runtime;
+using System;
+using System.IO;
 
 namespace Optuna.Artifacts
 {
     public class Artifacts
     {
-        public static string UploadArtifact(dynamic trial, string filePath, dynamic artifactStore)
+        private dynamic _artifactStore;
+
+        public void CreateFileSystemArtifactStore(dynamic optuna, string backendPath)
         {
-            dynamic optuna = Py.Import("optuna");
-            return optuna.artifacts.upload_artifact(trial, filePath, artifactStore);
+            if (string.IsNullOrEmpty(backendPath))
+            {
+                throw new ArgumentException("backendPath is null or empty");
+            }
+            if (!Directory.Exists(backendPath))
+            {
+                Directory.CreateDirectory(backendPath);
+            }
+            _artifactStore = optuna.artifacts.FileSystemArtifactStore(base_path: backendPath);
+        }
+
+        public string UploadArtifact(dynamic optuna, dynamic trial, string filePath)
+        {
+            if (string.IsNullOrEmpty(filePath))
+            {
+                throw new ArgumentException("filePath is null or empty");
+            }
+            if (!File.Exists(filePath))
+            {
+                throw new ArgumentException("filePath does not exist");
+            }
+            if (_artifactStore == null)
+            {
+                throw new ArgumentException("artifactStore is null. please call CreateFileSystemArtifactStore first.");
+            }
+            return optuna.artifacts.upload_artifact(trial, filePath, _artifactStore);
         }
     }
 }
