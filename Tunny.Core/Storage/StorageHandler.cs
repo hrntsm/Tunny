4b33578230041ba8f8fa1c0407bfe8db8d3415be
NATSUME Hiroaki <hiroaki@nature-architects.com>
2024-03-11 19:16:31 +0900

    Fix ci fail

--- Tunny.Core/Storage/StorageHandler.cs
+++ Tunny.Core/Storage/StorageHandler.cs
@@ -96,6 +96,5 @@ namespace Tunny.Core.Storage
 
             return storage;
         }
-
     }
 }
--- Tunny.CoreTests/Solver/OutputTests.cs
+++ Tunny.CoreTests/Solver/OutputTests.cs
@@ -13,41 +13,41 @@ namespace Tunny.Core.Solver.Tests
             _output = new Output("TestFile/journal.log");
         }
 
-        [Fact()]
+        [Fact]
         public void GetParetoFrontTrialTest()
         {
-            Trial[] trials = _output.GetTargetTrial(new int[] { -1 }, "output_test");
+            Trial[] trials = _output.GetTargetTrial(new [] { -1 }, "output_test");
             Assert.Equal(2, trials.Length);
         }
 
-        [Fact()]
+        [Fact]
         public void GetAllTargetTrialTest()
         {
-            Trial[] trials = _output.GetTargetTrial(new int[] { -10 }, "output_test");
+            Trial[] trials = _output.GetTargetTrial(new [] { -10 }, "output_test");
             Assert.Equal(10, trials.Length);
         }
 
-        [Fact()]
+        [Fact]
         public void GetSpecificTargetTrialTest()
         {
-            Trial[] trials = _output.GetTargetTrial(new int[] { 3, 2, 9 }, "output_test");
+            Trial[] trials = _output.GetTargetTrial(new [] { 3, 2, 9 }, "output_test");
             Assert.Equal(3, trials.Length);
             Assert.Equal(3, trials[0].Number);
             Assert.Equal(2, trials[1].Number);
             Assert.Equal(9, trials[2].Number);
         }
 
-        [Fact()]
+        [Fact]
         public void GetSpecificTargetTrialWithNullTest()
         {
-            Trial[] trials = _output.GetTargetTrial(new int[] { 100 }, "output_test");
+            Trial[] trials = _output.GetTargetTrial(new [] { 100 }, "output_test");
             Assert.Empty(trials);
         }
 
-        [Fact()]
+        [Fact]
         public void NonExistentStudyTest()
         {
-            Trial[] trials = _output.GetTargetTrial(new int[] { -1 }, "nonexistent_study");
+            Trial[] trials = _output.GetTargetTrial(new [] { -1 }, "nonexistent_study");
             Assert.Empty(trials);
         }
     }
