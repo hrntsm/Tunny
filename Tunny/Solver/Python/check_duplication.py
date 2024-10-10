7748eadbe79bf0ba85b8b0d21856b23b098b99f0
NATSUME Hiroaki <hiroaki@nature-architects.com>
2024-10-10 18:32:54 +0900

    Clean code

--- Tunny/Solver/Python/check_duplication.py
+++ Tunny/Solver/Python/check_duplication.py
@@ -9,11 +9,15 @@ def check_duplicate(trial):
     )
     for t in reversed(trials_to_consider):
         if trial.params == t.params:
-            trial.set_user_attr(
-                "NOTE",
-                f"trial {t.number} and trial {trial.number} were duplicate parameters.",
-            )
-            if "Constraint" in t.user_attrs:
-                trial.set_user_attr("Constraint", t.user_attrs["Constraint"])
+            set_attr_to_duplicate_trial(trial, t)
             return t.values
     return None
+
+
+def set_attr_to_duplicate_trial(base_trial, compared_trial) -> None:
+    base_trial.set_user_attr(
+        "NOTE",
+        f"trial {compared_trial.number} and trial {base_trial.number} were duplicate parameters.",
+    )
+    if "Constraint" in compared_trial.user_attrs:
+        base_trial.set_user_attr("Constraint", compared_trial.user_attrs["Constraint"])
